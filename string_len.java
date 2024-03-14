375ed943e1e660138099658531cb710f59b38c2e
Neha3010 <158296501+NehaBogoju@users.noreply.github.com>
2024-03-14 22:19:47 +0530

    Create string_len.java

---
+++ string_len.java
@@ -0,0 +1,23 @@
+/*Tom is playing scrabble and forms new words. He wants to count the number of characters present in the word formed by him. Help him to do this using the below java program.
+
+Code :
+*/
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+
+		String str=sc.next();
+
+		//find length and print the same 
+
+    int len=str.length();
+
+          System.out.println("The length of the word is "+len);		
+
+	}
+
+}
